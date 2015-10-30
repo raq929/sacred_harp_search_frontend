06def24eae3a5e8eb6de44d2917f415e9fe12344
racheltstevens <racheltstevens@gmail.com>
2015-10-30 16:38:14 -0400

    Creates display_data.js
    Links index to display_data.js

---
+++ display_data.js
@@ -0,0 +1,12 @@
+//takes a data set and displays it in the table
+var song_names = {
+  131: "Messiah",
+  146: "Hallelujah"
+};
+var data = {
+  caller: {first_name: "Rachel", surname: "Stevens"},
+  songs: [
+  {number: 146,singing: "Ohio State Convention",state: "OH",year: 2005},
+  {number: 131, singing: "Boston All-Day", state: "MA", year: 2014}]
+};
+
